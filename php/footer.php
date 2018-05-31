da3fa28dc478b235abe0d0938a5d3ed2c1b5554d
denningace <35429760+denningace@users.noreply.github.com>
2018-06-01 00:35:43 +0100

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,11 @@
+  
+<footer>
+
+    
+    <div id="footerCopyright">
+    © Copyright 2018
+Disclaimer: this website is not real, this is a college assignment.
+
+    </div>
+    
+</footer>
---
+++ php/header.php
@@ -0,0 +1,36 @@
+<header id="navHeader"> <!--all -->
+<!-- Hidden CSS switch
+     <div id="contactButton">
+         <div id="contact"><button type="button" id="btnCss"onclick="changeCSS('css/styleMain.css',0);" >CSS Main</button>
+    </div>
+    <div id="loginButton">
+        <div id="login"><div id="contactButton">
+            <button type="button" id="btnCss"onclick="changeCSS('css/styleAlt.css',0);" >CSS Alt</button>
+        </div>
+        </div>
+    </div>
+	-->
+        <div id="navMenuInfo">   
+        </div>    
+		
+            <div id="navMenu"> <!--Blue bottom-->
+                <nav id="navMenuLogo">
+                    <div id="logoNav"> <img id="logo" src="img/logo.png" alt="Fast Hosts"></div>
+                </nav>
+				
+                <nav id="navMenuLinks"> <!--bottom Links-->
+                    <ul>
+						
+                        <li class="homepageNav"> <a href="index.php">Home</a></li> <!--Home Page link-->
+                        
+                        <li class="domainsNav"> <a href="domaintoplevel.php">Top Level Domains</a></li> <!-- Domain Link-->
+                        
+                        <li class="registerNav"> <a href="registerinterest.php">Register Your Interest</a></li> <!--Register Interest-->
+                        
+                        <li class="aboutNav"> <a href="aboutfasthosts.php">About Fasthosts </a></li> <!-- About Link-->
+                        
+                        <li class="siteMapNav"> <a href="sitemap.php">Site Map</a></li> <!--Site Map Link-->
+                    </ul>
+                </nav>
+            </div>    
+</header>
