b606207fb7c0ba4aa2714c3c43f7f47aa83487c6
denningace <35429760+denningace@users.noreply.github.com>
2018-06-01 00:34:26 +0100

    Add files via upload

---
+++ domaintoplevel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+	<link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+    <link rel="shortcut icon" href="img/favicon.ico"/>
+    <title>Fasthosts | Hosting, Domains & servers </title>
+	
+<head>
+
+<body> 
+<?php include 'php/header.php'; ?>
+
+
+
+
+</body>
+
+<?php include 'php/footer.php'; ?>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+	<link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+    <link rel="shortcut icon" href="img/favicon.ico"/>
+    <title>Fasthosts | Hosting, Domains & servers </title>
+	<?php include 'php/footer.php'; ?>
+	<?php include 'php/header.php'; ?>
+</head>
+
+<body> 
+	<Section id="indexContainer">
+		<div id="indexArticleMain">
+		</div>
+			<div id="indexArticleContainer">
+				<div id="indexContainerTitle">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus at lorem vel tempor. Mauris mollis sodales ante id pulvinar.
+				</div>
+			
+				<div id="indexContainerContent">
+				 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet porta mollis. Pellentesque aliquet vestibulum nulla eu accumsan. In ultrices justo purus, tincidunt lacinia leo congue non. Aenean aliquam tempor ex. Pellentesque elementum egestas elementum. Duis fringilla ut mauris id tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas dignissim leo quam, non bibendum ipsum semper sed. Phasellus id bibendum urna, rutrum laoreet arcu. Aenean elementum felis a risus facilisis, a efficitur nulla molestie. 
+				 </div>
+			</div>
+		<div id="indexContainerButton">
+			<a href=""> Buy : £13.48</a>  
+		</div>
+		
+		
+	</section>
+	
+	<Section id="indexContainerBottom">
+		<article id="indexArticleBottom">
+		</article>
+		<article id="indexArticleBottom">
+		</article>
+		<article id="indexArticleBottom">
+		</article>
+	</section>
+</body>
+
+</html>
